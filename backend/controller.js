2fab3d5fdc7a8ccca169cbcbbb980974dc757826
Darlington Onwuama <darlingtonsomto@gmail.com>
2021-10-15 09:31:38 +0200

    feat: created react app using the boothstrap. Setting first view

--- backend/controller.js
+++ backend/controller.js
@@ -1,9 +1,19 @@
 'use strict';
 
+const site = [
+  {
+    "name": "site 1",
+    "speed": 8.5,
+    "temperature": 20,
+    "active power": 5.5,
+    "percentage active": 20,
+    "turbine event": "NC"
+  }
+]
+
 exports.getAll = async (req, res) => {
   try {
-    // database
-    res.status(200).json(topics);
+    res.status(200).json(site);
   } catch (e) {
     console.error(e);
     res.status(200);
---
+++ frontend/src/screens/overview.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react'
+
+const url = 'http://localhost:5000/site';
+
+const Overview = () => {
+
+  const [overview, setOverview] = useState([]);
+
+  useEffect(() => {
+    
+    
+    async function requestData () {
+      const response = await fetch(url);
+      const data = await response.json();
+      setOverview(data);
+    }
+  }, []);
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Overview
